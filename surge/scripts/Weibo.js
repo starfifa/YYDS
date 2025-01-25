c1b6350e53ee3cd0dc4aec306108f4f1dc0c412b
GitHub Action <action@github.com>
2025-01-25 11:13:39 +0800

    Auto Update 2025-01-25 11:13

--- surge/scripts/Weibo.js
+++ surge/scripts/Weibo.js
@@ -1,5 +1,5 @@
 // 引用地址：https://kelee.one/Resource/Script/Weibo/Weibo_remove_ads.js
-// 更新时间：2025-01-24 08:52:38
+// 更新时间：2025-01-25 08:48:55
 /*
 引用地址：https://raw.githubusercontent.com/RuCu6/Loon/main/Scripts/weibo.js
 */
--- surge/scripts/Xiaohongshu.js
+++ surge/scripts/Xiaohongshu.js
@@ -1,5 +1,5 @@
 // 引用地址：https://kelee.one/Resource/Script/RedPaper/RedPaper_remove_ads.js
-// 更新时间：2025-01-24 08:52:38
+// 更新时间：2025-01-25 08:48:55
 /*
 引用地址 https://raw.githubusercontent.com/RuCu6/Loon/main/Scripts/xiaohongshu.js
 */
