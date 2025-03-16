25f2f57c18895c40bf8d654e015b2b19d36fb500
GitHub Action <action@github.com>
2025-03-16 11:28:16 +0800

    Auto Update 2025-03-16 11:28

--- surge/scripts/Pinduoduo.js
+++ surge/scripts/Pinduoduo.js
@@ -1,5 +1,5 @@
 // 引用地址：https://kelee.one/Resource/Script/PinDuoDuo/PinDuoDuo_remove_ads.js
-// 更新时间：2025-03-15 08:55:16
+// 更新时间：2025-03-16 09:00:37
 // 脚本引用 https://raw.githubusercontent.com/RuCu6/Loon/refs/heads/main/Scripts/pdd.js
 // 2024-11-06 04:20
 
--- surge/scripts/Xiaohongshu.js
+++ surge/scripts/Xiaohongshu.js
@@ -1,5 +1,5 @@
 // 引用地址：https://kelee.one/Resource/Script/RedPaper/RedPaper_remove_ads.js
-// 更新时间：2025-03-15 08:55:16
+// 更新时间：2025-03-16 09:00:36
 /*
 引用地址 https://raw.githubusercontent.com/RuCu6/Loon/main/Scripts/xiaohongshu.js
 */
