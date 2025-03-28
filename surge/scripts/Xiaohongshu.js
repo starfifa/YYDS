4c5a1cb8f204b02006a66688f387e3e8212903b7
GitHub Action <action@github.com>
2025-03-28 11:29:18 +0800

    Auto Update 2025-03-28 11:29

--- surge/scripts/Xiaohongshu.js
+++ surge/scripts/Xiaohongshu.js
@@ -1,5 +1,5 @@
 // 引用地址：https://kelee.one/Resource/Script/RedPaper/RedPaper_remove_ads.js
-// 更新时间：2025-03-27 08:57:10
+// 更新时间：2025-03-28 08:57:14
 /*
 引用地址 https://raw.githubusercontent.com/RuCu6/Loon/main/Scripts/xiaohongshu.js
 */
